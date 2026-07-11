0010b97b3b9f5b53409f710f489ea0d71acde0ec
sai-chinni <chinnisaikrishna007@gmil.com>
2026-07-11 13:14:00 +0530

    Fix CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,8 +8,10 @@ app=FastAPI(title="AI Recipe Generater API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[ "http://localhost:5173",
-        "https://ai-recipe-generator-ebon.vercel.app",],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://ai-recipe-generator-ebon.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
