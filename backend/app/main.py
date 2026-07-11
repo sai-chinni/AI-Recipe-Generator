7ae6ae29778b8ad873e4e9dc4990178b2f0d80dc
sai-chinni <chinnisaikrishna007@gmil.com>
2026-07-11 13:01:38 +0530

    add vercel url to cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,7 +9,7 @@ app=FastAPI(title="AI Recipe Generater API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[ "http://localhost:5173",
-        "http://127.0.0.1:5173",],
+        "https://ai-recipe-generator-ebon.vercel.app/",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
