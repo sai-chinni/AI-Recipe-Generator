3b8ebe0d1b43f77d1180e45b09660a4a60053451
sai-chinni <chinnisaikrishna007@gmil.com>
2026-07-11 14:28:19 +0530

    chanded url

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,7 +10,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://ai-recipe-generator-ebon.vercel.app",
+        "https://ai-recipe-generator-1-bndh.onrender.com",
     ],
     allow_credentials=False,
     allow_methods=["*"],
