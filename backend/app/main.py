d6e53c5fa42858c207d97ffca5289ff55b280d12
sai-chinni <chinnisaikrishna007@gmil.com>
2026-07-11 13:46:05 +0530

    deleted test_db

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,7 @@ app.add_middleware(
         "http://localhost:5173",
         "https://ai-recipe-generator-ebon.vercel.app",
     ],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
